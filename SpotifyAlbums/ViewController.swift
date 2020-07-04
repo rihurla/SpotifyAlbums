2e0d690473e5d03bc7a6ed29d79d19ea07903b93
Ricardo Hurla <rihurla@Ricardos-MacBook-Pro.local>
2020-07-04 14:58:50 +0100

    Project Init

---
+++ SpotifyAlbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyAlbums
+//
+//  Created by Ricardo Hurla on 04/07/2020.
+//  Copyright © 2020 Ricardo Hurla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
